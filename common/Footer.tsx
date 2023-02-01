9843db0cccd80956dd7f7b46f3e2969f586f3ac9
Exxempt <46376436+Exxempt@users.noreply.github.com>
2023-02-01 14:57:08 -0600

    Update Footer.tsx

--- common/Footer.tsx
+++ common/Footer.tsx
@@ -26,56 +26,15 @@ export const Footer = ({
     >
       <div className="flex w-full flex-wrap items-start justify-between gap-10 py-10">
         <div className="flex items-center">
-          <img
-            alt={bgColor}
-            className="inline-block h-[28px]"
-            src={
-              contrastColorMode(bgColor)[1]
-                ? '/cardinal-crosshair.svg'
-                : '/cardinal-crosshair-dark.svg'
-            }
-          />
-          <span
-            className="ml-3 text-2xl font-semibold"
-            style={{ color: lighten(0.4, contrastColorMode(bgColor)[0]) }}
-          >
-            Cardinal
-          </span>
+
         </div>
         <div className="flex gap-10 self-end text-center md:gap-20">
           <span className="flex flex-col items-start gap-1">
-            <div
-              className="mb-2 text-lg font-semibold"
-              style={{ color: lighten(0.4, contrastColorMode(bgColor)[0]) }}
-            >
-              App
-            </div>
-            <a href="/" className="text-gray-400">
-              Pools
-            </a>
-            <a href="/admin" className="text-gray-400">
-              Admin
-            </a>
+
           </span>
           <span className="flex flex-col items-start gap-1">
-            <div
-              className="mb-2 text-lg font-semibold"
-              style={{ color: lighten(0.4, contrastColorMode(bgColor)[0]) }}
-            >
-              Resources
-            </div>
-            <a href="https://docs.cardinal.so/" className="text-gray-400">
-              Documentation
-            </a>
-            <a
-              href="https://github.com/cardinal-labs"
-              className="text-gray-400"
-            >
-              Github
-            </a>
-            <a href="mailto:team@cardinal.so" className="text-gray-400">
-              Contact
-            </a>
+
+
             {/*<a href="" className="text-gray-400">
               Privacy
             </a> */}
@@ -108,23 +67,7 @@ export const Footer = ({
         style={{ borderColor: lighten(0.2, bgColor) }}
       >
         <div className="flex items-center justify-center gap-2 text-gray-400">
-          Powered by Cardinal
-        </div>
-        <div className="flex gap-4 text-gray-200">
-          {Object.entries(SOCIALS).map(([id, { icon, link }]) => {
-            return (
-              <a
-                key={id}
-                href={link}
-                target="_blank"
-                rel="noreferrer"
-                style={{ color: accentColor }}
-                className={`opacity-80 transition-opacity hover:text-primary hover:opacity-100`}
-              >
-                {icon}
-              </a>
-            )
-          })}
+
         </div>
       </div>
       {/* <div className="text-md flex flex-row justify-center font-medium">
